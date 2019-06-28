cd9ab45ebafcb76c8aafa3e3c5332b93bf092c8e
Ho Young Jhoo <hoyoung.jhoo@furiosa.ai>
2019-06-28 11:46:11 +0900

    implement sa layer

--- model/sa_layer.py
+++ model/sa_layer.py
@@ -4,19 +4,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
-
-def _double(num):
-    if type(num) == int:
-        return (num, num)
-    elif type(num) == tuple and len(num) == 1:
-        return (num[0], num[0])
-    else:
-        return num
+from torch.nn.modules.utils import _pair
 
 class SelfAttentionConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size,
-                 stride=1, padding=0, dilation=1, groups=1,
-                 bias=True, padding_mode='zeros'):
+                 stride=1, padding=0, groups=1, bias=True):
         super(SelfAttentionConv2d, self).__init__()
         if in_channels % groups != 0:
             raise ValueError('in_channels must be divisible by groups')
@@ -24,12 +16,10 @@ class SelfAttentionConv2d(nn.Module):
             raise ValueError('out_channels must be divisible by groups')
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.kernel_size = _double(kernel_size)
-        self.stride = _double(stride)
-        self.padding = _double(padding)
-        self.dilation = _double(dilation)
+        self.kernel_size = _pair(kernel_size)
+        self.stride = _pair(stride)
+        self.padding = _pair(padding)
         self.groups = groups
-        self.padding_mode = padding_mode
 
         self.weight = nn.Parameter(torch.Tensor(
                 out_channels, in_channels // groups, *kernel_size))
@@ -39,12 +29,12 @@ class SelfAttentionConv2d(nn.Module):
             self.register_parameter('bias', None)
 
         self.rel_size = out_channels // 2
-        self.relative_x = nn.Parameter(torch.Tensor(self.rel_size))
-        self.relative_y = nn.Parameter(torch.Tensor(out_channels - self.rel_size))
+        self.relative_x = [nn.Parameter(torch.Tensor(self.rel_size)) for _ in self.kernel_size[1]]
+        self.relative_y = [nn.Parameter(torch.Tensor(out_channels - self.rel_size)) for _ in self.kernel_size[0]]
 
-        self.weight_query = [nn.Parameter(torch.Tensor(out_channels // groups, in_channels // groups)) for _ in range(groups)]
-        self.weight_key = [nn.Parameter(torch.Tensor(out_channels // groups, in_channels // groups)) for _ in range(groups)]
-        self.weight_value = [nn.Parameter(torch.Tensor(out_channels // groups, in_channels // groups)) for _ in range(groups)]
+        self.weight_query = nn.Parameter(torch.Tensor(groups, in_channels // groups, out_channels // groups))
+        self.weight_key = nn.Parameter(torch.Tensor(groups, in_channels // groups, out_channels // groups))
+        self.weight_value = nn.Parameter(torch.Tensor(groups, in_channels // groups, out_channels // groups))
 
         self.reset_parameters()
 
@@ -54,19 +44,60 @@ class SelfAttentionConv2d(nn.Module):
         init.kaiming_uniform_(self.weight_value, a=math.sqrt(5))
 
         if self.bias is not None:
-            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
-            bound = 1 / math.sqrt(fan_in)
+            bound = 1 / math.sqrt(self.out_channels)
             init.uniform_(self.bias, -bound, bound)
 
-        init.uniform_(self.relative_x, 1 / math.sqrt(self.rel_size))
-        init.uniform_(self.relative_y, 1 / math.sqrt(self.out_channels - self.rel_size))
+        rel_bound = 1 / math.sqrt(self.rel_size)
+        for rel_x in self.relative_x:
+            init.uniform_(rel_x, -rel_bound, rel_bound)
+        for rel_y in self.relative_y:
+            init.uniform_(rel_y, -rel_bound, rel_bound)
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        kh, kw = self.kernel
+
+        fh = (h + self.padding[0] * 2 - self.kernel[0]) // self.stride[0] + 1
+        fw = (w + self.padding[1] * 2 - self.kernel[1]) // self.stride[1] + 1
+        fc = self.out_channels
+
+        # TODO: check this could be moved to init
+        relative_pos = []
+        for rel_x in self.relative_x:
+            for rel_y in self.relative_y:
+                relative_pos.append(torch.cat(rel_x, rel_y))
+        relative_pos = torch.stack(relative_pos, dim=0).view(self.kernel_size[0]*self.kernel_size[1], self.out_channels)
+
+        win = F.unfold(x, self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
+        win = win.transpose(2, 1).view(b, fh*fw, c, kh*kw).transpose(3, 2).view(b, fh*fw, kh, kw, c)
+        win_query = win[:, :, (kh-1)//2, (kw-1)//2, :]
+
+        v_q = win_query.matmul(self.weight_query).view(b, fh*fw, 1, fc)
+        v_k = win.matmul(self.weight_key).view(b, fh*fw, kh*kw, fc)
+        v_v = win.matmul(self.weight_value).view(b, fh*fw, kh*kw, fc)
+
+        v_ab = v_q * (v_k + relative_pos)
+        v_x = F.softmax(v_ab, dim=2)
+
+        v = (v_x * v_v).sum(dim=2)
+        v = v.transpose(2, 1).view(b, fc, fh, fw)
+
+        if self.bias is not None:
+            v += self.bias
+
+        return v
+
+
+class SelfAttentionBottleneck(nn.Module):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, base_width=64, dilation=1):
+        super(SelfAttentionBottleneck, self).__init__()
 
     def forward(self, x):
         return x
 
-class SelfAttentionBlock(nn.Module):
+class SelfAttentionStem(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, base_width=64, dilation=1):
-        super(SelfAttentionBlock, self).__init__()
+        super(SelfAttentionStem, self).__init__()
 
     def forward(self, x):
         return x
--- model/sa_resnet.py
+++ model/sa_resnet.py
@@ -4,31 +4,34 @@ import torch.nn.functional as F
 
 import torchvision.models.resnet as resnet
 
-from sa_layer import SelfAttentionConv2d, SelfAttentionBlock
+from sa_layer import SelfAttentionConv2d, SelfAttentionBottleneck
 
 model_dict = {
-    'sa_resnet18': (sa_resnet, [2, 2, 2, 2]),
-    'sa_resnet34': (sa_resnet, [2, 2, 2, 2]),
-    'sa_resnet50': (sa_resnet, [2, 2, 2, 2]),
-    'sa_resnet101': (sa_resnet, [2, 2, 2, 2]),
-    'sa_resnet152': (sa_resnet, [2, 2, 2, 2]),
-    'cstem_sa_resnet18': (cstem_sa_resnet, [2, 2, 2, 2]),
-    'cstem_sa_resnet34': (cstem_sa_resnet, [2, 2, 2, 2]),
-    'cstem_sa_resnet50': (cstem_sa_resnet, [2, 2, 2, 2]),
-    'cstem_sa_resnet101': (cstem_sa_resnet, [2, 2, 2, 2]),
-    'cstem_sa_resnet152': (cstem_sa_resnet, [2, 2, 2, 2]),
+    'resnet26': (bottleneck_resnet, resnet.Bottleneck, [1, 2, 4, 1]),
+    'resnet38': (bottleneck_resnet, resnet.Bottleneck, [2, 3, 5, 2]),
+    'sa_resnet26': (sa_resnet, SelfAttentionBottleneck, [1, 2, 4, 1]),
+    'sa_resnet38': (sa_resnet, SelfAttentionBottleneck, [2, 3, 5, 2]),
+    'sa_resnet50': (sa_resnet, SelfAttentionBottleneck, [3, 4, 6, 3]),
+    'sa_resnet101': (sa_resnet, SelfAttentionBottleneck, [3, 4, 23, 3]),
+    'sa_resnet152': (sa_resnet, SelfAttentionBottleneck, [3, 8, 36, 3]),
+    'cstem_sa_resnet26': (cstem_sa_resnet, SelfAttentionBottleneck, [1, 2, 4, 1]),
+    'cstem_sa_resnet38': (cstem_sa_resnet, SelfAttentionBottleneck, [2, 3, 5, 2]),
+    'cstem_sa_resnet50': (cstem_sa_resnet, SelfAttentionBottleneck, [3, 4, 6, 3]),
+    'cstem_sa_resnet101': (cstem_sa_resnet, SelfAttentionBottleneck, [3, 4, 23, 3]),
+    'cstem_sa_resnet152': (cstem_sa_resnet, SelfAttentionBottleneck, [3, 8, 36, 3]),
 }
 
 model_names = list(model_dict.keys())
 
 def get_model(arch, **kwargs):
-    model_fn, layers = model_dict[arch]
-    return model_fn(SelfAttentionBlock, layers, **kwargs)
+    model_fn, block, layers = model_dict[arch]
+    return model_fn(block, layers, **kwargs)
+
+def bottleneck_resnet(block, layers, **kwargs):
+    return resnet.ResNet(block, layers, **kwargs)
 
 def sa_resnet(block, layers, **kwargs):
-    model = resnet.ResNet(block, layers, **kwargs)
-    return model
+    return resnet.ResNet(block, layers, **kwargs)
 
 def cstem_sa_resnet(block, layers, **kwargs):
-    model = resnet.ResNet(block, layers, **kwargs)
-    return model
+    return resnet.ResNet(block, layers, **kwargs)
